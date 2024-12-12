d110d7c57a0da59974819be8bd960fde2a0361e6
sbuzzard7 <s.buzzard7@outlook.com>
2024-12-12 13:00:10 -0500

    Add files via upload

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Private Members
+
+        private MarkType[] mResults;
+        private bool mPlayer1Turn;
+        private bool mGameEnded;
+
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+
+            mPlayer1Turn = true;
+
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            mGameEnded = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (mGameEnded)
+            {
+                NewGame();
+                return;
+            }
+
+            var button = (Button)sender;
+
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var index = column + (row * 3);
+
+            if (mResults[index] != MarkType.Free)
+                return;
+
+            mResults[index] = mPlayer1Turn ? MarkType.Cross : MarkType.Nought;
+
+            button.Content = mPlayer1Turn ? "X" : "O";
+
+            if (!mPlayer1Turn)
+                button.Foreground = Brushes.Red;
+
+            mPlayer1Turn ^= true;
+
+            CheckForWinner();
+ 
+        }
+
+        private void CheckForWinner()
+        {
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[1] & mResults[2]) == mResults[0])
+            {
+                mGameEnded = true;
+                Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Yellow;
+            }
+
+            if (mResults[3] != MarkType.Free && (mResults[3] & mResults[4] & mResults[5]) == mResults[3])
+            {
+                mGameEnded = true;
+                Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Yellow;
+            }
+
+            if (mResults[6] != MarkType.Free && (mResults[6] & mResults[7] & mResults[8]) == mResults[6])
+            {
+                mGameEnded = true;
+                Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Yellow;
+            }
+
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+            {
+                mGameEnded = true;
+                Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Yellow;
+            }
+
+            if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+            {
+                mGameEnded = true;
+                Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Yellow;
+            }
+
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+            {
+                mGameEnded = true;
+                Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Yellow;
+            }
+
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[4] & mResults[8]) == mResults[0])
+            {
+                mGameEnded = true;
+                Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Yellow;
+            }
+
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[4] & mResults[6]) == mResults[2])
+            {
+                mGameEnded = true;
+                Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Yellow;
+            }
+
+
+            if (!mResults.Any(result => result == MarkType.Free))
+            {
+                mGameEnded = true;
+
+                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                {
+                    button.Background = Brushes.DarkGray;
+                });
+            }
+
+        }
+    }
+}
---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToe
+
+    /// <summary>
+    /// The type of value a cell in thge game is currently at 
+    /// Free is not clicked
+    /// Nought is O
+    /// Cross is X
+    /// </summary>
+{
+    public enum MarkType
+    {
+        Free,
+        Nought,
+        Cross
+    }
+}
